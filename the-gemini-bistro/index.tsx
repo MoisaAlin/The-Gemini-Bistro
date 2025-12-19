87273a2f6ac11532b4e4ff6e8d590995baa4702c
MoisaAlin <108398314+MoisaAlin@users.noreply.github.com>
2025-12-19 15:28:43 +0200

    Update ReactDOM import to use createRoot

--- the-gemini-bistro/index.tsx
+++ the-gemini-bistro/index.tsx
@@ -1,5 +1,6 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { initializeMonitoring } from './services/monitoringService';
 import SentryErrorBoundary from './components/SentryErrorBoundary';
@@ -13,7 +14,7 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <SentryErrorBoundary>
